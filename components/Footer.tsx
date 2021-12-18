012c3bcbc35043fc7e9dc23ad33937880577dc61
Kartik Sorathiy <kartik@solvative.com>
2021-12-18 03:44:45 -0500

    initial setup and core homepage

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { globals } from '../globals';
+import React from "react";
+import { globals } from "../globals";
 
 export const Footer: React.FC = () => (
   <div className="footer">
-    <p>{`© ${globals.yourName} ${new Date().getFullYear()}`}</p>
+    {/* <p>{`© ${globals.yourName} ${new Date().getFullYear()}`}</p>
     <a href="/rss.xml">
       <img src="/img/rss-white.svg" alt="RSS Feed" height="30" width="30" />
-    </a>
+    </a> */}
   </div>
 );
--- globals.ts
+++ globals.ts
@@ -1,11 +1,12 @@
 export namespace globals {
-  export const yourName = 'Alyssa P. Hacker';
-  export const siteName = 'My Awesome Blog';
-  export const siteDescription = 'I write about code \'n stuff';
-  export const siteCreationDate = 'March 3, 2020 04:00:00 GMT';
-  export const twitterHandle = '@alyssaphacker';
-  export const email = 'alyssa@example.com';
-  export const url = 'https://alyssaphacker.com';
-  export const accentColor = '#4fc2b4';
-  export const googleAnalyticsId = ``; // e.g. 'UA-999999999-1'
+  export const yourName = "Kartik S. Sorathiya";
+  export const siteName = "My digital reflection!";
+  export const siteDescription =
+    "I understand what you want, build what you need, and deliver what you deserve.";
+  export const siteCreationDate = "December 18, 2020 07:41:00 GMT";
+  export const twitterHandle = "@alyssaphacker";
+  export const email = "sorathiyakartik@gmail.com";
+  export const url = "https://kartiksorathiya.com";
+  export const accentColor = "#4fc2b4";
+  export const googleAnalyticsId = `UA-177190245-1`; // e.g. 'UA-999999999-1'
 }
