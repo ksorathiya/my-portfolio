a79be130840b319d219f77b24608cb786f38371c
Kartik Sorathiya <sorathiyakartik@gmail.com>
2021-12-19 23:18:21 -0500

    add section for spotify playlist

--- globals.ts
+++ globals.ts
@@ -8,5 +8,5 @@ export namespace globals {
   export const email = "sorathiyakartik@gmail.com";
   export const url = "https://kartiksorathiya.com";
   export const accentColor = "#4fc2b4";
-  export const googleAnalyticsId = `UA-177190245-1`; // e.g. 'UA-999999999-1'
+  export const googleAnalyticsId = `G-CT5T5FBXCQ`; // e.g. 'UA-999999999-1'
 }
