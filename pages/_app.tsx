cd21fab686978c9141ff44399aaa49b290515e96
Kartik Sorathiya <sorathiyakartik@gmail.com>
2021-12-20 22:02:28 -0500

    serve umami over my domain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ const App: React.FC = ({ Component, pageProps }: any) => {
           async
           defer
           data-website-id="cdae2032-c587-40f3-b564-80f15e53f19a"
-          src="https://portfolio-analytics.vercel.app/umami.js"
+          src="https://analytics.sorathiyakartik.com/umami.js"
         ></script>
         {globals.googleAnalyticsId && (
           <script
